a4aeeb5c574f2dee314b1087b0d5b5957553f427
손창현 <gganzii1215@gmail.com>
2022-04-15 21:24:12 +0900

    feat: user sign up

--- server/src/main/java/com/tooliv/server/domain/user/application/UserServiceImpl.java
+++ server/src/main/java/com/tooliv/server/domain/user/application/UserServiceImpl.java
@@ -3,15 +3,15 @@ package com.tooliv.server.domain.user.application;
 import com.tooliv.server.domain.user.application.dto.request.SignUpRequestDTO;
 import com.tooliv.server.domain.user.domain.User;
 import com.tooliv.server.domain.user.domain.repository.UserRepository;
-import com.tooliv.server.global.config.ModelMapperConfig;
 import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
-import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+
+//    private final PasswordEncoder passwordEncoder;
 
     private final UserRepository userRepository;
 
@@ -21,6 +21,7 @@ public class UserServiceImpl implements UserService {
             .email(signUpRequestDTO.getEmail())
             .name(signUpRequestDTO.getName())
             .nickname(signUpRequestDTO.getNickname())
+//            .password(passwordEncoder.encode(signUpRequestDTO.getPassword()))
             .password(signUpRequestDTO.getPassword())
             .createdAt(LocalDateTime.now())
             .code(signUpRequestDTO.getCode()).build();
