184e1237292bcbf7f39a3d7ef65a20152e398cb9
손창현 <gganzii1215@gmail.com>
2022-04-17 02:17:00 +0900

    add: security - JwtAccessDeniedHandler

---
+++ server/src/main/java/com/tooliv/server/global/security/config/SecurityConfig.java
@@ -0,0 +1,12 @@
+package com.tooliv.server.global.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
+
+}
