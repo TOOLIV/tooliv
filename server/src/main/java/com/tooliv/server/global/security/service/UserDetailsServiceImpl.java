b6b04749433d2a54b009e6b1dc33fa5ba7aaa460
손창현 <gganzii1215@gmail.com>
2022-04-17 02:34:52 +0900

    add: security - UserDetailServiceImpl

---
+++ server/src/main/java/com/tooliv/server/global/security/service/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.tooliv.server.global.security.service;
+
+import com.tooliv.server.domain.user.domain.User;
+import com.tooliv.server.domain.user.domain.repository.UserRepository;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String userAccount) throws UsernameNotFoundException {
+
+        Optional<User> oUser = userRepository.findByEmailAndDeletedAt(userAccount, null);
+
+        return new UserDetailsImpl(
+            oUser.orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다.")));
+
+    }
+
+}
