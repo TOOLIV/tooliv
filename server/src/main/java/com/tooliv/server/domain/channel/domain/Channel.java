271bd9de1a0e46db248483598dbe9fe5cb5d2dbe
DayeLEE <ldy1853@naver.com>
2022-04-18 13:38:11 +0900

    [S06P31A402-87] feat: Add Channel, Workspace Entity

---
+++ server/src/main/java/com/tooliv/server/domain/channel/domain/Channel.java
@@ -0,0 +1,38 @@
+package com.tooliv.server.domain.channel.domain;
+
+import com.tooliv.server.domain.user.domain.User;
+import com.tooliv.server.domain.workspace.domain.Workspace;
+import com.tooliv.server.global.common.BaseEntity;
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Channel extends BaseEntity {
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "private_yn")
+    private boolean privateYn;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "video_yn")
+    private boolean videoYn;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "workspace_id")
+    private Workspace workspace;
+
+}
---
+++ server/src/main/java/com/tooliv/server/domain/workspace/domain/Workspace.java
@@ -0,0 +1,37 @@
+package com.tooliv.server.domain.workspace.domain;
+
+import com.tooliv.server.domain.user.domain.User;
+import com.tooliv.server.global.common.BaseEntity;
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Workspace extends BaseEntity {
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "url")
+    private String url;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "owner_id")
+    private User user;
+
+}
