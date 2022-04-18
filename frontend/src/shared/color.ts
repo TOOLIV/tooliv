480cd78c440a3dcae2fb8e10abd1088b4f5f13c4
JINMYEONG <1995jjm@naver.com>
2022-04-18 23:48:10 +0900

    add: color pallette

---
+++ frontend/src/shared/color.ts
@@ -0,0 +1,23 @@
+export const colors = {
+  primary: "#FF9E89",
+  secondary: "#FF7585",
+  third: "#FFD089",
+  lightPrimaryLight: "#FFBFB0",
+  lightSecondary: "#FFAAB4",
+
+  gray25: "#F8F9FA",
+  gray50: "#F1F3F5",
+  gray100: "#E9ECEF",
+  gray200: "#DEE2E6",
+  gray300: "#CED4DA",
+  gray400: "#ADB5BD",
+  gray500: "#868E96",
+  gray600: "#4A5056",
+  gray700: "#3D4248",
+  gray800: "#31363B",
+  gray900: "#292D32",
+
+  black: "1B1F24",
+
+  lightGreenA700: "#64DD17",
+};
