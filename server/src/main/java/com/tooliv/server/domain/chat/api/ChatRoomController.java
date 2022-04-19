f55e548bfc35279ae1458bde227eaa958480ec4b
JeongHongJin <mistake9518@gmail.com>
2022-04-19 16:53:30 +0900

    [S06P31A402-45] feat: redis config

--- server/src/main/java/com/tooliv/server/domain/chat/api/ChatRoomController.java
+++ server/src/main/java/com/tooliv/server/domain/chat/api/ChatRoomController.java
@@ -23,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/chat")
 public class ChatRoomController {
+
     private final ChatService chatService;
     private final UserService userService;
 
