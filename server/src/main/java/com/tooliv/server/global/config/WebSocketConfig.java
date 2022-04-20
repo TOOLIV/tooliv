763261a228660816d1452535796c4a262fe16d6f
JeongHongJin <mistake9518@gmail.com>
2022-04-20 18:54:36 +0900

    [S06P31A402-44] fix: socketconfiguration with cors

--- server/src/main/java/com/tooliv/server/global/config/WebSocketConfig.java
+++ server/src/main/java/com/tooliv/server/global/config/WebSocketConfig.java
@@ -1,6 +1,9 @@
 package com.tooliv.server.global.config;
 
+import com.tooliv.server.global.util.stomp.StompHandler;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -8,10 +11,13 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 
 @Configuration
 @EnableWebSocketMessageBroker
+@RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    private final StompHandler stompHandler;
+
     @Override
-    public void configureMessageBroker(final MessageBrokerRegistry registry) {
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
         // prefix 설정
         // 발행 pub, 구독 sub
         registry.enableSimpleBroker("/sub");
@@ -19,11 +25,18 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     }
 
     @Override
-    public void registerStompEndpoints(final StompEndpointRegistry registry) {
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
         // Cors 설정
         // Stomp endPoint chatting
         registry.addEndpoint("/chatting")
-            .setAllowedOrigins("*")
+            .setAllowedOriginPatterns("*")
             .withSockJS();
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+
+        registration.interceptors(stompHandler);
+
     }
 }
