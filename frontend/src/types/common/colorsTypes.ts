b2a684481e3f8c16147081562fba524cda3a7c47
thdalstn6352 <thdalstn6352@naver.com>
2022-04-20 19:09:25 +0900

    add:input & badge atom

--- frontend/src/types/common/colorsTypes.ts
+++ frontend/src/types/common/colorsTypes.ts
@@ -1,24 +1,28 @@
 export type colorsTypes = {
   color:
-    | "primary"
-    | "secondary"
-    | "third"
-    | "lightPrimary"
-    | "lightSecondary"
-    | "darkPrimary"
-    | "gray25"
-    | "gray50"
-    | "gray100"
-    | "gray200"
-    | "gray300"
-    | "gray400"
-    | "gray500"
-    | "gray600"
-    | "gray700"
-    | "gray800"
-    | "gray900"
-    | "black"
-    | "white"
-    | "lightGreenA700"
-    | "red700";
+    | 'primary'
+    | 'secondary'
+    | 'third'
+    | 'lightPrimary'
+    | 'lightSecondary'
+    | 'darkPrimary'
+    | 'gray25'
+    | 'gray50'
+    | 'gray100'
+    | 'gray200'
+    | 'gray300'
+    | 'gray400'
+    | 'gray500'
+    | 'gray600'
+    | 'gray700'
+    | 'gray800'
+    | 'gray900'
+    | 'black'
+    | 'white'
+    | 'lightGreenA700'
+    | 'red700';
+};
+
+export type colorObjTypes = {
+  [key: string]: string;
 };
--- frontend/src/types/common/iconsTypes.ts
+++ frontend/src/types/common/iconsTypes.ts
@@ -1,27 +1,28 @@
-import { colorsTypes } from "./colorsTypes";
+import { colorsTypes } from './colorsTypes';
 
 export type iconsTypes = {
   icon:
-    | "lock"
-    | "public"
-    | "person"
-    | "setting"
-    | "plus"
-    | "videoOn"
-    | "videoOff"
-    | "monitor"
-    | "audioOn"
-    | "audioOff"
-    | "anglesLeft"
-    | "anglesRight"
-    | "addPerson"
-    | "shareMonitor"
-    | "xMark"
-    | "exit";
+    | 'lock'
+    | 'public'
+    | 'person'
+    | 'setting'
+    | 'plus'
+    | 'videoOn'
+    | 'videoOff'
+    | 'monitor'
+    | 'audioOn'
+    | 'audioOff'
+    | 'anglesLeft'
+    | 'anglesRight'
+    | 'addPerson'
+    | 'shareMonitor'
+    | 'xMark'
+    | 'exit';
+  color?: colorsTypes['color'];
   onClick?: () => void;
 };
 
 export type largeIconTypes = {
-  icon: iconsTypes["icon"];
-  color?: colorsTypes["color"];
+  icon: iconsTypes['icon'];
+  color?: colorsTypes['color'];
 };
