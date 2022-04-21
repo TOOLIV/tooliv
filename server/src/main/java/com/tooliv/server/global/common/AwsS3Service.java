153a1b275239095ddb5bfe61061a52755b5302dd
손창현 <gganzii1215@gmail.com>
2022-04-22 01:53:16 +0900

    add: AwsS3ServiceImpl

--- server/src/main/java/com/tooliv/server/global/common/AwsS3Service.java
+++ server/src/main/java/com/tooliv/server/global/common/AwsS3Service.java
@@ -1,5 +1,12 @@
 package com.tooliv.server.global.common;
 
+import org.springframework.web.multipart.MultipartFile;
+
 public interface AwsS3Service {
 
+    String uploadImage(MultipartFile multipartFile);
+
+    String createFileName(String fileName);
+
+    String getFileExtension(String fileName);
 }
