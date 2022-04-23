bc4ec59d6022762d2b5289d56626b1ef19efbf07
DayeLEE <ldy1853@naver.com>
2022-04-24 01:09:16 +0900

    feat: Add GetWorkspaceMember API

--- server/src/main/java/com/tooliv/server/domain/workspace/application/WorkspaceMemberService.java
+++ server/src/main/java/com/tooliv/server/domain/workspace/application/WorkspaceMemberService.java
@@ -1,16 +1,15 @@
 package com.tooliv.server.domain.workspace.application;
 
-import com.tooliv.server.domain.channel.application.dto.request.DeleteChannelMemberRequestDTO;
-import com.tooliv.server.domain.channel.application.dto.request.RegisterChannelMemberRequestDTO;
 import com.tooliv.server.domain.workspace.application.dto.request.DeleteWorkspaceMemberRequestDTO;
-import com.tooliv.server.domain.workspace.application.dto.request.ModifyWorkspaceRequestDTO;
 import com.tooliv.server.domain.workspace.application.dto.request.RegisterWorkspaceMemberRequestDTO;
-import com.tooliv.server.domain.workspace.application.dto.request.RegisterWorkspaceRequestDTO;
+import com.tooliv.server.domain.workspace.application.dto.response.WorkspaceMemberListGetResponseDTO;
 
 public interface WorkspaceMemberService {
 
     void addWorkspaceMember(RegisterWorkspaceMemberRequestDTO registerWorkspaceMemberRequestDTO);
 
     void deleteWorkspaceMember(DeleteWorkspaceMemberRequestDTO deleteWorkspaceMemberRequestDTO);
+
+    WorkspaceMemberListGetResponseDTO getWorkspaceMemberList(String workspaceId);
 
 }
