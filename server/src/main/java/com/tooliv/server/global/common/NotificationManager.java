b980de178334e8cba05577b599611fb6354932c3
손창현 <gganzii1215@gmail.com>
2022-04-25 15:46:33 +0900

    add: NotificationManager

---
+++ server/src/main/java/com/tooliv/server/global/common/NotificationManager.java
@@ -0,0 +1,20 @@
+package com.tooliv.server.global.common;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NotificationManager {
+    private Logger log = LoggerFactory.getLogger(NotificationManager.class);
+
+    @Autowired
+    private MatterMostSender mmSender;
+
+    public void sendNotification(Exception e, String uri, String params) {
+        log.info("#### SEND Notification");
+        mmSender.sendMessage(e, uri, params);
+    }
+
+}
