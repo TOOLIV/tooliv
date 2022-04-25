64bb1274cfb1311e82ec419587014c4bebdc0db7
jubi in <thetruthrunswild@naver.com>
2022-04-25 17:56:43 +0900

    style: custom scrollbar

--- client/src/organisms/chat/Messages.tsx
+++ client/src/organisms/chat/Messages.tsx
@@ -6,9 +6,27 @@ import { channelContents } from '../../recoil/atom';
 import { colors } from '../../shared/color';
 import { contentTypes } from '../../types/channel/contentType';
 const Container = styled.div`
-  width: 100%;
+  width: calc(100% + 38px);
   height: 100%;
   overflow-y: auto;
+  padding-right: 32px;
+  /* 스크롤바 설정*/
+  &::-webkit-scrollbar {
+    width: 6px;
+  }
+
+  /* 스크롤바 막대 설정*/
+  &::-webkit-scrollbar-thumb {
+    height: 17%;
+    background-color: ${colors.gray300};
+    /* 스크롤바 둥글게 설정    */
+    border-radius: 10px;
+  }
+
+  /* 스크롤바 뒷 배경 설정*/
+  &::-webkit-scrollbar-track {
+    background-color: rgba(0, 0, 0, 0);
+  }
 `;
 
 const Messages = () => {
