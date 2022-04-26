67cfe80aa761ce087d1584eb35cd0e360b451f1b
손창현 <gganzii1215@gmail.com>
2022-04-26 14:01:32 +0900

    refactor: email check (user)

--- server/src/main/java/com/tooliv/server/domain/user/api/UserController.java
+++ server/src/main/java/com/tooliv/server/domain/user/api/UserController.java
@@ -7,6 +7,7 @@ import com.tooliv.server.domain.user.application.dto.request.LogInRequestDTO;
 import com.tooliv.server.domain.user.application.dto.request.NicknameUpdateRequestDTO;
 import com.tooliv.server.domain.user.application.dto.response.LogInResponseDTO;
 import com.tooliv.server.domain.user.application.dto.response.NicknameResponseDTO;
+import com.tooliv.server.domain.user.exception.NotUniqueEmailException;
 import com.tooliv.server.global.common.BaseResponseDTO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -88,6 +90,23 @@ public class UserController {
         }
 
         return ResponseEntity.status(201).body(BaseResponseDTO.of("프로필 이미지 등록 성공"));
+    }
+
+    @GetMapping("/check/{email}")
+    @ApiOperation(value = "이메일 중복 체크")
+    @ApiResponses({
+        @ApiResponse(code = 200, message = "이메일 사용 가능"),
+        @ApiResponse(code = 409, message = "이메일 사용 불가"),
+    })
+    public ResponseEntity<? extends BaseResponseDTO> checkEmail(
+        @PathVariable("email") @ApiParam(value = "이메일", required = true) String email) {
+        try {
+            userService.checkEmail(email);
+        } catch (NotUniqueEmailException e) {
+            return ResponseEntity.status(409).body(BaseResponseDTO.of("이메일 사용 불가"));
+        }
+
+        return ResponseEntity.status(200).body(BaseResponseDTO.of("이메일 사용 가능"));
     }
 
     @GetMapping("/search")
--- server/src/main/java/com/tooliv/server/domain/user/application/service/UserService.java
+++ server/src/main/java/com/tooliv/server/domain/user/application/service/UserService.java
@@ -19,6 +19,8 @@ public interface UserService {
 
     void uploadProfileImage(MultipartFile multipartFile);
 
+    void checkEmail(String email);
+
     UserListResponseDTO getUserList(String keyword);
 
     User getCurrentUser();
