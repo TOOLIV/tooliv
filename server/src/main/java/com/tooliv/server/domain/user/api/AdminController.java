880ffcf813d8898e522d9b13f720e43581b1be2a
손창현 <gganzii1215@gmail.com>
2022-04-28 03:11:10 +0900

    refactor: delete NotUniqueEmailException

--- server/src/main/java/com/tooliv/server/domain/user/api/AdminController.java
+++ server/src/main/java/com/tooliv/server/domain/user/api/AdminController.java
@@ -1,25 +1,20 @@
 package com.tooliv.server.domain.user.api;
 
-import com.tooliv.server.domain.user.application.dto.request.SignUpRequestDTO;
 import com.tooliv.server.domain.user.application.dto.request.UserCodeUpdateRequestDTO;
 import com.tooliv.server.domain.user.application.dto.response.UserListResponseDTO;
 import com.tooliv.server.domain.user.application.service.AdminService;
-import com.tooliv.server.domain.user.exception.NotUniqueEmailException;
 import com.tooliv.server.global.common.BaseResponseDTO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
-import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
