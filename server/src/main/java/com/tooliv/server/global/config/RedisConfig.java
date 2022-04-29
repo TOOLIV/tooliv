31a7e19ad6914516527624f6365ddeb789933e9f
JeongHongJin <mistake9518@gmail.com>
2022-04-29 13:36:54 +0900

    fix: redis configuration

--- server/src/main/java/com/tooliv/server/global/config/RedisConfig.java
+++ server/src/main/java/com/tooliv/server/global/config/RedisConfig.java
@@ -23,8 +23,8 @@ public class RedisConfig {
     @Value("${spring.redis.port}")
     private int redisPort;
 
-    @Value("${spring.redis.password}")
-    private String password;
+//    @Value("${spring.redis.password}")
+//    private String password;
 
 
     @Bean
@@ -32,7 +32,7 @@ public class RedisConfig {
         RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
         redisStandaloneConfiguration.setHostName(redisHost);
         redisStandaloneConfiguration.setPort(redisPort);
-        redisStandaloneConfiguration.setPassword(password);
+//        redisStandaloneConfiguration.setPassword(password);
         return new LettuceConnectionFactory(redisStandaloneConfiguration);
     }
 
