d99f0fd6ac4b2231b7f6d1bab6cd4b57b5c26bee
JeongHongJin <mistake9518@gmail.com>
2022-04-29 13:19:30 +0900

    fix: redis properties

--- server/src/test/java/com/tooliv/server/ServerApplicationTests.java
+++ server/src/test/java/com/tooliv/server/ServerApplicationTests.java
@@ -9,7 +9,14 @@ class ServerApplicationTests {
 
     @Test
     void contextLoads() {
-
+        System.out.println(jasyptEncoding("k6a402.p.ssafy.io"));
+    }
+    public String jasyptEncoding(String value) {
+        String key = "tooliv-jasypt";
+        StandardPBEStringEncryptor pbeEnc = new StandardPBEStringEncryptor();
+        pbeEnc.setAlgorithm("PBEWithMD5AndDES");
+        pbeEnc.setPassword(key);
+        return pbeEnc.encrypt(value);
     }
 
 }
