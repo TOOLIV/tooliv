e56722d5fe2556c63c09f0321cbd19c70b212291
JeongHongJin <mistake9518@gmail.com>
2022-05-04 13:29:14 +0900

    fix: notification DTO workspaceId add

--- server/src/main/java/com/tooliv/server/domain/channel/application/dto/response/NotificationInfoDTO.java
+++ server/src/main/java/com/tooliv/server/domain/channel/application/dto/response/NotificationInfoDTO.java
@@ -17,8 +17,8 @@ public class NotificationInfoDTO {
     @ApiModelProperty("채널 List 목록")
     private String channelId;
 
-    @ApiModelProperty("유저 List 목록")
-    private String userId;
+    @ApiModelProperty("워크스페이스 List 목록")
+    private String workspaceId;
 
     @ApiModelProperty("알람 List 목록")
     private boolean notificationRead;
