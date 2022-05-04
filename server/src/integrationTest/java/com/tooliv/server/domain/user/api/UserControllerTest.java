9ac277d834df77500fb4733ebe97d507247ba7d7
손창현 <gganzii1215@gmail.com>
2022-05-05 03:11:14 +0900

    add: testcontainers testImplementations

--- server/src/integrationTest/java/com/tooliv/server/domain/user/api/UserControllerTest.java
+++ server/src/integrationTest/java/com/tooliv/server/domain/user/api/UserControllerTest.java
@@ -26,14 +26,18 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
+@Testcontainers
 public class UserControllerTest {
 
     @Autowired
