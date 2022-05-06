d3f0980e1d603f39c63473bcb48b1def51444c60
손창현 <gganzii1215@gmail.com>
2022-05-06 11:19:39 +0900

    add: ProfileInfoResponseDTO

---
+++ server/src/main/java/com/tooliv/server/domain/user/application/dto/response/ProfileInfoResponseDTO.java
@@ -0,0 +1,29 @@
+package com.tooliv.server.domain.user.application.dto.response;
+
+import com.tooliv.server.global.common.BaseResponseDTO;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Builder;
+import lombok.Getter;
+
+@ApiModel("ProfileInfoResponseDTO")
+@Getter
+@Builder
+public class ProfileInfoResponseDTO extends BaseResponseDTO {
+
+    @ApiModelProperty("회원 닉네임")
+    private String nickname;
+
+    @ApiModelProperty("회원 프로필 이미지")
+    private String profileImage;
+
+    public static ProfileInfoResponseDTO of(String message, ProfileInfoResponseDTO profileInfoResponseDTO) {
+        profileInfoResponseDTO.setMessage(message);
+
+        return profileInfoResponseDTO;
+    }
+
+    public void updateProfileImage(String url) {
+        this.profileImage = url;
+    }
+}
