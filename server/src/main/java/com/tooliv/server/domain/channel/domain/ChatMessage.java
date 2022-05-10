7143fa24c380587a7ef2acb39821a7d55a5c8d9d
JeongHongJin <mistake9518@gmail.com>
2022-05-10 14:09:57 +0900

    feat: chatMessage entity create

--- server/src/main/java/com/tooliv/server/domain/channel/domain/ChatMessage.java
+++ server/src/main/java/com/tooliv/server/domain/channel/domain/ChatMessage.java
@@ -2,8 +2,12 @@ package com.tooliv.server.domain.channel.domain;
 
 import com.tooliv.server.domain.user.domain.User;
 import com.tooliv.server.global.common.BaseEntity;
+import io.swagger.annotations.ApiModelProperty;
+import java.io.Serializable;
 import java.time.LocalDateTime;
 import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -19,30 +23,33 @@ import lombok.ToString;
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
-public class ChatMessage extends BaseEntity {
+public class ChatMessage  {
+
+    @EmbeddedId
+    private Chat chat;
 
     // 내용
     @Column(name = "content")
     private String content;
 
+    @Column(name = "updated")
+    private boolean updated;
+
+    @Column(name = "deleted")
+    private boolean deleted;
+
     // 시간
     @Column(name = "created_at")
     private LocalDateTime sendTime;
 
-    // 채팅방 => 채널방으로 변경의 여지가 있다.
-    @ManyToOne
-    @JoinColumn(name = "channel_id")
-    private Channel channel;
+    @Embeddable
+    public static class Chat implements Serializable {
 
-    // 작성자
-    @ManyToOne
-    @JoinColumn(name = "user_id")
-    private User sender;
+        @Column(name = "chat_id")
+        private long chatId;
 
-    @Column(name = "type")
-    private MessageType type; // 메시지 타입
-
-    public enum MessageType {
-        ENTER, QUIT, TALK
+        @Column(name = "channel_id")
+        private String channelId;
     }
+
 }
