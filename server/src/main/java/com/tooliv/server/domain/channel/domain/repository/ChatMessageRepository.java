b1048d9368d3bf1e6c77d04c00f8e2570ea1f05e
JeongHongJin <mistake9518@gmail.com>
2022-05-11 16:12:30 +0900

    fix: ChatMessageRepository Param annotation add

--- server/src/main/java/com/tooliv/server/domain/channel/domain/repository/ChatMessageRepository.java
+++ server/src/main/java/com/tooliv/server/domain/channel/domain/repository/ChatMessageRepository.java
@@ -1,6 +1,7 @@
 package com.tooliv.server.domain.channel.domain.repository;
 
 import com.tooliv.server.domain.channel.domain.ChatMessage;
+import io.lettuce.core.dynamic.annotation.Param;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,7 +14,7 @@ public interface ChatMessageRepository extends JpaRepository<ChatMessage, String
     @Query(value="SELECT * "
         + "FROM chat_message c "
         + "WHERE c.content LIKE %:searchContent%", nativeQuery = true)
-    Optional<List<ChatMessage>> findByContents(String searchContent);
+    Optional<List<ChatMessage>> findByContents(@Param("searchContent") String searchContent);
 
     Optional<ChatMessage> findByChatChatIdAndChatChannelId(long chatId,String channelId);
 }
