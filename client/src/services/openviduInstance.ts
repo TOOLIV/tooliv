ac744d388e7ff76983660dea4b6e29e83ad4b12a
손창현 <gganzii1215@gmail.com>
2022-05-17 08:35:54 +0900

    refactor: Dockerfile

--- client/src/services/openviduInstance.ts
+++ client/src/services/openviduInstance.ts
@@ -5,7 +5,7 @@ const baseURL = localStorage.getItem('baseURL');
 
 export const openviduInstance = axios.create({
   baseURL: !isElectron()
-    ? window.env.BASE_URL + ':4443'
+    ? window.env.BASE_URL
     : JSON.parse(baseURL!).url + ':4443',
   // baseURL: 'https://k6a402.p.ssafy.io:4443',
   headers: {
--- client/src/services/wsconnect.ts
+++ client/src/services/wsconnect.ts
@@ -18,7 +18,7 @@ const baseURL = localStorage.getItem('baseURL');
 let sockJS = baseURL
   ? new SockJS(`${JSON.parse(baseURL).url}/chatting`)
   : // 로컬에서 테스트시 REACT_APP_TEST_URL, server 주소는 REACT_APP_BASE_SERVER_URL
-    new SockJS(`${process.env.REACT_APP_BASE_SERVER_URL}/chatting`);
+    new SockJS(`${window.env.BASE_URL}/chatting`);
 export let client: Stomp.Client = Stomp.over(sockJS);
 let subscribe: Stomp.Subscription;
 
