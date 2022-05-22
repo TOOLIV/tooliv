f588c93929e651625fc8705ddb27e4681cc33565
손창현 <gganzii1215@gmail.com>
2022-05-23 04:22:37 +0900

    refactor: reservation

--- server-community/src/main/java/com/tooliv/server/global/common/MatterMostSender.java
+++ server-community/src/main/java/com/tooliv/server/global/common/MatterMostSender.java
@@ -1,6 +1,7 @@
 package com.tooliv.server.global.common;
 
 import com.google.gson.Gson;
+import com.tooliv.server.domain.channel.application.dto.request.WebhookMessageRequestDTO;
 import com.tooliv.server.global.common.MatterMostMessageDTO.Attachment;
 import com.tooliv.server.global.common.MatterMostMessageDTO.Attachments;
 import lombok.RequiredArgsConstructor;
@@ -33,6 +34,9 @@ public class MatterMostSender {
         }
 
         try {
+            WebhookMessageRequestDTO webhookMessageRequestDTO = WebhookMessageRequestDTO.builder()
+                .webhook_id("039a1907-5ff5-406d-ba7d-e09338d59eb5").build()
+
             Attachment attachment = Attachment.builder()
                 .channel(mmProperties.getChannel())
                 .authorIcon(mmProperties.getAuthorIcon())
